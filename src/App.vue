323fa53f69f498af04ac99fdd47ce4d8fee22855
srh919 <89631009+srh919@users.noreply.github.com>
2023-06-02 17:55:25 -0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Chessboard />
+  </div>
+</template>
+
+<script>
+import Chessboard from "@/components/Chessboard.vue";
+
+export default {
+  name: "App",
+  components: {
+    Chessboard,
+  },
+};
+</script>
