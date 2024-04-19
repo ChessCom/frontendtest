1dcdb7e5176e34cceb5cdde865c6fbebd741a5c9
Kevin Godoi <kevingodoi95@gmail.com>
2024-04-19 18:54:59 -0300

    create vue app

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import SideMenu from './components/SideMenu.vue'
+import ChessCanvas from './components/ChessboardCanvas.vue'
+</script>
+
+<template>
+  <main id="main">
+    <ChessCanvas />
+    <SideMenu />
+  </main>
+</template>
+
+<style scoped>
+#main {
+  height: 100vh;
+  display: grid;
+  grid-template-columns: auto;
+  grid-template-rows: 55% 45%;
+}
+
+@media only screen and (min-width: 768px) {
+  #main {
+    height: 100vh;
+    grid-template-columns: 3fr 1fr;
+    grid-template-rows: 1fr;
+  }
+}
+</style>
