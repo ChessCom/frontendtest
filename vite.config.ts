cec95f5c2891882eaf3b8371d7e86756fea3ba98
Kevin Godoi <kevingodoi95@gmail.com>
2024-04-19 21:21:47 -0300

    Add config to use frontendtest as default

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,8 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    VueDevTools(),
-  ],
+  base: '/frontendtest/',
+  plugins: [vue(), vueJsx(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
