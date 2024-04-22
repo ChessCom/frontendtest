005d041418487e74cbab9004d7519d91c57fd8d7
Kevin Godoi <kevingodoi95@gmail.com>
2024-04-22 06:30:05 -0300

    Add new styles and changed how isSelected works

--- src/components/TableItem.vue
+++ src/components/TableItem.vue
@@ -1,5 +1,5 @@
-<template>
-  <div :class="['table-item', { isClickDisable: isClickDisabled }]">
+<template :id="`item-${clickIndex}`">
+  <div :class="['table-item', { isClickDisable: isClickDisabled, isSelected: isSelected }]">
     <div class="row-number" v-if="clickIndex">{{ clickIndex }}.</div>
     <div class="row-square">{{ square }}</div>
   </div>
@@ -7,29 +7,36 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 
-const { clickIndex, square } = defineProps<{
+const { clickIndex, square, isSelected } = defineProps<{
   clickIndex?: number
   isClickDisabled?: boolean
   square: string
+  isSelected?: boolean
 }>()
 </script>
 <style scoped>
 .table-item {
-  cursor: pointer;
-  display: flex;
-  gap: 0.5rem;
+  position: relative;
   box-sizing: border-box;
-  color: var(--c-white-soft);
+  width: 100%;
+  height: 30px;
+  padding: 0 5px 0 15px;
+  overflow: hidden;
   font-size: 13px;
   font-weight: 600;
-  height: 30px;
   line-height: 30px;
-  padding: 0 5px 0 15px;
-  width: 100%;
+  color: var(--c-white);
+  cursor: pointer;
 }
-.table-item:nth-child(even) {
+
+.row-number {
+  width: 3rem;
+}
+
+.table-item:nth-child(odd) {
   background-color: var(--color-background-soft);
 }
+
 .table-item:hover {
   color: var(--c-white);
   background-color: var(--color-background-mute);
@@ -37,5 +44,24 @@ const { clickIndex, square } = defineProps<{
 
 .isClickDisable {
   pointer-events: none;
+}
+
+.row-square {
+  position: absolute;
+  top: 3px;
+  left: 40px;
+  min-width: 40px;
+  height: 24px;
+  line-height: 24px;
+  padding: 0 4px;
+}
+
+.isSelected > .row-square {
+  background-color: #484745;
+  border-color: #5a5b58;
+  border-bottom-style: solid;
+  border-bottom-width: 3px;
+  border-radius: 2px;
+  cursor: default;
 }
 </style>
